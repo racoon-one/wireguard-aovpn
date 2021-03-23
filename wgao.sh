e70ca67563b0b5767ed3c9cba44baec0ca1a8c6d
racoon-one <71573664+racoon-one@users.noreply.github.com>
2021-03-23 11:48:22 +0100

    Update wgao.sh

--- wgao.sh
+++ wgao.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ssid=" " #Insert your WLAN SSID here
-vpn_config_file=" " #/etc/wireguard name of the config file you want to use (without the file extension).
+ssid=" " # Insert your WLAN SSID here
+vpn_config_file=" " # Name of the config file in a/etc/wireguard/ you want to use (without the file extension).
 
 if (( $( iwgetid -r ) == $ssid )) 
 then 
